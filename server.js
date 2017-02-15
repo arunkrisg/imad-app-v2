12c0a13e2e7c09662da0a10a83e1a7b6203868c5
arunkrisg <g.arunkrishnan@gmail.com>
2017-02-15 19:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/:articleName', function(req, res) {
   //articleName == article-one
   //articles[articleName] == {} content object for article one
@@ -116,11 +122,6 @@ app.get('/article-three', function(req, res) {
 });
 */
 
-var counter = 0;
-app.get('/conuter', function (req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
