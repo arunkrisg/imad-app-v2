9a98b8a102be56b11db2fc375aa12d2521d7d380
arunkrisg <g.arunkrishnan@gmail.com>
2017-02-16 12:27:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,4 +60,6 @@ submit.onlclick = function () {
     for (var i = 0; i < names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
+    var ul = document.getElementById ("namelist");
+    ul.innerHTML = list;
 }
