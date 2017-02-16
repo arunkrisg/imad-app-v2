369176a07bebcfe8e5a3340e628c25d28694bc1f
arunkrisg <g.arunkrishnan@gmail.com>
2017-02-16 12:50:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,7 @@ submit.onlclick = function () {
     
     //Capture a lsit of names and render it as a list
     var names = ['name1','name2','name3','name4'];
-    var lsit = '';
+    var list = '';
     for (var i = 0; i < names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
