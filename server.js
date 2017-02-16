b6a20d0bb5921c7d09de3ef6a52e61eb140b4058
arunkrisg <g.arunkrishnan@gmail.com>
2017-02-16 15:37:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,15 +100,23 @@ app.get('/counter', function (req, res) {
    res.send(counter.toString());
 });
 
+/*
 var names = [];
 app.get('/submit-name/:name', function(req, res) {
     // Get the name from the request
         var name = req.params.name;
         names.push(name);
-        
         // JSON - Java Script Object Notation
         res.send(JSON.stringify(names));
-        
+});
+*/
+var names = [];
+app.get('/submit-name', function(req, res) { //submit-name?name-xxxx
+    // Get the name from the request
+        var name = req.query.name;
+        names.push(name);
+        // JSON - Java Script Object Notation
+        res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function(req, res) {
